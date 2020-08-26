4d6135387002e822c7679f35b89877d750e4061b
Bryce <bryce.rankin@gmail.com>
2020-08-25 19:07:44 -0700

    HW 2.2 Complete

--- Homework/src/Nuke2.java
+++ Homework/src/Nuke2.java
@@ -1,4 +1,22 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Nuke2 {
+
+    public static void main(String[] args) throws Exception {
+
+        // Creates string object to store keyboard input, listens for input from user
+        String inputString;
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        inputString = keyboard.readLine();
+
+        // Concatenates first character with all characters after second character
+        // This excludes the second character in the string
+        String test1 = inputString.substring(0,1) + inputString.substring(2);
+
+        // Prints string with second character removed
+        System.out.println(test1);
+
+
+    }
 }
