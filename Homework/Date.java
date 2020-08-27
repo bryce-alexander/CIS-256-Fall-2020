f0ce441d85c38b23a90b6919dd08932e06c3addc
Bryce <bryce.rankin@gmail.com>
2020-08-26 20:55:38 -0700

    Part III in HW2: Need fix on method calculations

--- Homework/Date.java
+++ Homework/Date.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 class Date {
 
   /* Put your private data fields here. */
+  private int month;
+  private int day;
+  private int year;
 
   /** Constructs a date with the given month, day and year.   If the date is
    *  not valid, the entire program will halt with an error message.
@@ -14,7 +17,13 @@ class Date {
    *  @param year is the year in question, with no digits omitted.
    */
   public Date(int month, int day, int year) {
-
+    if (Date.isValidDate(month, day, year)!=true) {
+      System.out.println("This is not a valid date.  Please try again.");
+      System.exit(0);
+    }
+    this.month = month;
+    this.day = day;
+    this.year = year;
   }
 
   /** Constructs a Date object corresponding to the given string.
@@ -24,6 +33,14 @@ class Date {
    *  a valid date, the program halts with an error message.
    */
   public Date(String s) {
+    if (!s.matches("\\d{1,2}/\\d{1,2}/\\d{1,4}")) {
+      System.out.println("This is not a valid date.  Please enter with format MM/DD/YYYY");
+      System.exit(0);
+    }
+    String[] dateArray = s.split("/");
+    this.month = Integer.parseInt(dateArray[0]);
+    this.day = Integer.parseInt(dateArray[1]);
+    this.year = Integer.parseInt(dateArray[2]);
 
   }
 
@@ -62,9 +79,9 @@ class Date {
       }
     }
     if (Date.isLeapYear(year)) {
-
+      return 29;
     }
-    return 0;                           // replace this line with your solution
+    return 28;                           // replace this line with your solution
   }
 
   /** Checks whether the given date is valid.
@@ -73,6 +90,12 @@ class Date {
    *  Years prior to A.D. 1 are NOT valid.
    */
   public static boolean isValidDate(int month, int day, int year) {
+    if (month<1 || month>12 || year<1 || day<1 || day>31) {
+      return false;
+    }
+    else if (day>daysInMonth(month, year)) {
+      return false;
+    }
     return true;                        // replace this line with your solution
   }
 
@@ -82,21 +105,41 @@ class Date {
    *  @return a String representation of this date.
    */
   public String toString() {
-    return "stuff";                     // replace this line with your solution
+    return (this.month + "/" + this.day + "/" + this.year);                     // replace this line with your solution
   }
 
   /** Determines whether this Date is before the Date d.
    *  @return true if and only if this Date is before d. 
    */
   public boolean isBefore(Date d) {
-    return true;                        // replace this line with your solution
+    return (this.year * this.month * this.day)<(d.year*d.month*d.day);
+/*    if ((this.year * this.month * this.day)<(d.year*d.month*d.day)){
+      return true;
+    }*/
+/*    if (this.year<d.year) {
+      return true;
+    }
+    else if (this.year>d.year) {
+      return false;
+    }
+    else if (this.month<d.month) {
+      return true;
+    }
+    else if (this.month>d.month) {
+      return false;
+    }
+    else if (this.day<d.day) {
+      return true;
+    }*/
+    //return false;                        // replace this line with your solution
   }
 
   /** Determines whether this Date is after the Date d.
    *  @return true if and only if this Date is after d. 
    */
   public boolean isAfter(Date d) {
-    return true;                        // replace this line with your solution
+    return (this.year * this.month * this.day)<(d.year*d.month*d.day);
+                      // replace this line with your solution
   }
 
   /** Returns the number of this Date in the year.
@@ -114,7 +157,7 @@ class Date {
    *  @return the difference in days between d and this date.
    */
   public int difference(Date d) {
-    return 0;                           // replace this line with your solution
+    return (this.year * this.month * this.day)-(d.year*d.month*d.day);                           // replace this line with your solution
   }
 
   public static void main(String[] argv) {
@@ -175,10 +218,30 @@ class Date {
     System.out.println(d5 + " - " + d4  + " should be 48762: " + 
                        d5.difference(d4));
 
+    System.out.println("\nisLeapYear test: ");
     System.out.println("1800 (false): " + Date.isLeapYear(1800));
     System.out.println("1900 (false): " + Date.isLeapYear(1900));
     System.out.println("1600 (true): " + Date.isLeapYear(1600));
     System.out.println("2000 (true): " + Date.isLeapYear(2000));
     System.out.println("400 (true): " + Date.isLeapYear(400));
+
+    System.out.println("\ndaysInMonth test: ");
+    System.out.println("October of any year (31): " + Date.daysInMonth(10,1988));
+    System.out.println("July of any year (31): " + Date.daysInMonth(7,1921));
+    System.out.println("April of any year (30): " + Date.daysInMonth(4,1934));
+    System.out.println("November of any year (30): " + Date.daysInMonth(11,1900));
+    System.out.println("February of leap year 2020 (29): " + Date.daysInMonth(2,2020));
+    System.out.println("February of  non leap year 2019 (28): " + Date.daysInMonth(2,2019));
+
+    System.out.println("\nisValidDate test: ");
+    System.out.println("4/31/2020 (false): " + Date.isValidDate(4,31,2020));
+    System.out.println("5/30/2020 (true): " + Date.isValidDate(5,30,2020));
+    System.out.println("2/29/2020 (true): " + Date.isValidDate(2,29,2020));
+    System.out.println("2/29/2019 (false): " + Date.isValidDate(2,29,2019));
+    System.out.println("0/28/2019 (false): " + Date.isValidDate(0,28,2019));
+    System.out.println("4/32/2019 (false): " + Date.isValidDate(4,32,2019));
+    System.out.println("13/12/2019 (false): " + Date.isValidDate(13,12,2019));
+    System.out.println("3/0/2019 (false): " + Date.isValidDate(3,0,2019));
+
   }
 }
